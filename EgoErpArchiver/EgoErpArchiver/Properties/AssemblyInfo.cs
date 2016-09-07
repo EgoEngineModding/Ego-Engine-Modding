b73459ce7c498fb9b93103e36db8e4fd5e6a42da
Ryder25 <tasevp@yahoo.com>
2016-09-07 14:18:44 -0700

    ERP Archiver 4.2

--- EgoErpArchiver/EgoErpArchiver/Properties/AssemblyInfo.cs
+++ EgoErpArchiver/EgoErpArchiver/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.2016.0905")]
-[assembly: AssemblyFileVersion("4.1.2016.0905")]
+[assembly: AssemblyVersion("4.2.2016.0906")]
+[assembly: AssemblyFileVersion("4.2.2016.0906")]
