f1dae320e799ba535e1e65f70a027428b81677fc
Petar Tasev <ptasev@gmail.com>
2026-01-18 21:25:23 -0800

    Changes after file moves

--- src/EgoPssgEditor/App.axaml.cs
+++ src/EgoPssgEditor/App.axaml.cs
@@ -1,22 +1,51 @@
-﻿using System.Globalization;
-using System.Threading;
-using System.Windows;
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
 
-namespace EgoPssgEditor
+using EgoPssgEditor.ViewModels;
+using EgoPssgEditor.Views;
+
+namespace EgoPssgEditor;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public static new App? Current => (App?)Application.Current;
+    
+    public override void Initialize()
     {
-        public App()
-        {
-            var culture = new CultureInfo("en-US");
+        AvaloniaXamlLoader.Load(this);
+    }
 
-            Thread.CurrentThread.CurrentCulture = culture;
-            Thread.CurrentThread.CurrentUICulture = culture;
-            CultureInfo.DefaultThreadCurrentCulture = culture;
-            CultureInfo.DefaultThreadCurrentUICulture = culture;
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
+            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+            DisableAvaloniaDataAnnotationValidation();
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = new MainViewModel(),
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+
+    private void DisableAvaloniaDataAnnotationValidation()
+    {
+        // Get an array of plugins to remove
+        var dataValidationPluginsToRemove =
+            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
+
+        // remove each entry found
+        foreach (var plugin in dataValidationPluginsToRemove)
+        {
+            BindingPlugins.DataValidators.Remove(plugin);
         }
     }
 }
--- src/EgoPssgEditor/Views/MainWindow.axaml.cs
+++ src/EgoPssgEditor/Views/MainWindow.axaml.cs
@@ -1,9 +1,14 @@
-﻿using EgoPssgEditor.ViewModel;
-using System;
-using System.Diagnostics;
-using System.Windows;
+﻿using EgoPssgEditor.ViewModels;
 
-namespace EgoPssgEditor
+using System.Diagnostics;
+
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+
+using EgoEngineLibrary.Avalonia;
+
+namespace EgoPssgEditor.Views
 {
     // Copy/Paste; Search; ModelViewer;
     // 1.7.2 - Quick Float4 Fix To Make work with all textures
@@ -22,21 +27,92 @@ namespace EgoPssgEditor
     /// </summary>
     public partial class MainWindow : Window
     {
-        readonly MainViewModel view;
+        private MainViewModel? view;
+        private IDisposable? _fileOpenInteractionRegistration;
+        private IDisposable? _fileSaveInteractionRegistration;
+        
         public MainWindow()
         {
             InitializeComponent();
-            view = this.DataContext as MainViewModel;
         }
 
-        private void websiteMenuItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void websiteMenuItem_Click(object sender, RoutedEventArgs e)
         {
             Process.Start(new ProcessStartInfo("https://petar.page/l/ego-epe-home") { UseShellExecute = true });
         }
 
-        private void sourceCodeMenuItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void sourceCodeMenuItem_Click(object sender, RoutedEventArgs e)
         {
             Process.Start(new ProcessStartInfo("https://petar.page/l/ego-epe-code") { UseShellExecute = true });
+        }
+
+        protected override void OnDataContextChanged(EventArgs e)
+        {
+            view = this.DataContext as MainViewModel;
+            
+            // Dispose any old handler
+            _fileOpenInteractionRegistration?.Dispose();
+            _fileSaveInteractionRegistration?.Dispose();
+
+            if (view is not null)
+            {
+                // register the interaction handler
+                _fileOpenInteractionRegistration = view.FileOpenInteraction.RegisterHandler(FileOpenHandler);
+                _fileSaveInteractionRegistration = view.FileSaveInteraction.RegisterHandler(FileSaveHandler);
+            }
+
+            base.OnDataContextChanged(e);
+        }
+        
+        private async Task<string?> FileOpenHandler(FileOpenOptions openOptions)
+        {
+            // Get a reference to our TopLevel (in our case the parent Window)
+            var topLevel = GetTopLevel(this);
+            if (topLevel is null)
+            {
+                return null;
+            }
+
+            var options = new FilePickerOpenOptions
+            {
+                Title = openOptions.Title,
+                FileTypeFilter = openOptions.FileTypeChoices,
+                SuggestedFileType = openOptions.SuggestedFileType,
+                AllowMultiple = openOptions.AllowMultiple,
+                SuggestedFileName = openOptions.FileName,
+                SuggestedStartLocation = openOptions.InitialDirectory is null
+                    ? null
+                    : await topLevel.StorageProvider.TryGetFolderFromPathAsync(openOptions.InitialDirectory),
+            };
+
+            var storageFiles = await topLevel.StorageProvider.OpenFilePickerAsync(options);
+            return storageFiles.Count <= 0 ? null : storageFiles[0].Path.LocalPath;
+        }
+        
+        private async Task<string?> FileSaveHandler(FileSaveOptions saveOptions)
+        {
+            // Get a reference to our TopLevel (in our case the parent Window)
+            var topLevel = GetTopLevel(this);
+            if (topLevel is null)
+            {
+                return null;
+            }
+
+            var options = new FilePickerSaveOptions
+            {
+                Title = saveOptions.Title,
+                FileTypeChoices = saveOptions.FileTypeChoices,
+                SuggestedFileType = saveOptions.SuggestedFileType,
+                DefaultExtension = saveOptions.DefaultExtension,
+                ShowOverwritePrompt = saveOptions.ShowOverwritePrompt,
+                SuggestedFileName = saveOptions.FileName,
+                SuggestedStartLocation = saveOptions.InitialDirectory is null
+                    ? null
+                    : await topLevel.StorageProvider.TryGetFolderFromPathAsync(saveOptions.InitialDirectory),
+            };
+
+            var storageFiles = await topLevel.StorageProvider.SaveFilePickerAsync(options);
+            return storageFiles?.Path.LocalPath;
         }
     }
 }
