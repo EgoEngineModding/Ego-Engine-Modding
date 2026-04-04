02c7ecac0a85dbfb1b4d54a042cfc47ae9e40585
Petar Tasev <ptasev@gmail.com>
2026-04-04 13:31:33 -0700

    Add dialog framework

---
+++ src/EgoEngineLibrary.Frontend/Dialogs/Custom/Dialog.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+
+namespace EgoEngineLibrary.Frontend.Dialogs.Custom;
+
+public static class Dialog
+{
+    public static IMessenger Messenger { get; set; } = Messaging.Messenger.Default;
+
+    public static async Task<T?> ShowDialog<T>(DialogViewModel<T> viewModel)
+    {
+        viewModel.ResetResult();
+        await Messenger.Send(new DialogShowMessage(viewModel));
+        return viewModel.DialogResult;
+    }
+}
