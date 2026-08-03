158ed06be83695fa72ba1fa8d6d473dce35b8eab
Petar Tasev <ptasev@gmail.com>
2026-08-02 22:19:06 -0700

    Use common styles

--- src/EgoEngineLibrary.Frontend/Dialogs/MessageBox/MessageBoxButtonViewModel.cs
+++ src/EgoEngineLibrary.Frontend/Dialogs/MessageBox/MessageBoxButtonViewModel.cs
@@ -2,14 +2,6 @@
 
 public class MessageBoxButtonViewModel
 {
-    public static readonly MessageBoxButtonViewModel Ok = new MessageBoxButtonViewModel()
-    {
-        Text = "Ok",
-        Result = MessageBoxResult.OK,
-        IsDefault = true,
-        IsCancel = false,
-    };
-
     public string Text { get; init; } = "";
     
     public MessageBoxResult Result { get; init; }
